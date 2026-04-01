79ae456094480dfae90411e1e6dffdd2f9c8ea64
Sergiot3s <sergio@triesse.com.br>
2026-04-01 14:47:30 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,11 +2,7 @@ document.getElementById("leadForm").addEventListener("submit", async (e) => {
   e.preventDefault();
 
   const formData = new FormData(e.target);
-  const data = {
-    nome: formData.get('nome'),
-    email: formData.get('email'),
-    empresa: formData.get('empresa')
-  };
+  const data = Object.fromEntries(formData);
 
   console.log("Lead capturado:", data);
   alert("Recebido! Vamos entrar em contato.");
